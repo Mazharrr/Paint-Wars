a40cfc60177885cb08ec52b42891683390670c48
Maz <maz@Mazs-MacBook-Pro.local>
2017-02-01 17:43:54 -0500

    someComponents

---
+++ browser/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom'
+
+export default class App extends Component{
+    render(){
+
+      return (
+
+        <div>
+        </div>
+      )
+    }
+}
--- browser/index.js
+++ browser/index.js
@@ -1,3 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import {Provider} from 'react-redux'
+import store from './store'
+import App from './components/App'
+
 import 'pixi';
 import 'p2';
 import 'phaser';
@@ -5,4 +11,3 @@ import 'phaser';
 
 import game from './states/stateManager'
 import store from './store'
-// console.log('hello')
