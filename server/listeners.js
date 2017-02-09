a320729f01546a1192876a03b8a59cb55aa80b19
Maz <mazharrr@live.com>
2017-02-09 14:18:25 -0500

    random crates fix

--- server/listeners.js
+++ server/listeners.js
@@ -1,9 +1,48 @@
 const store = require ('./store')
 const {getClientData, deletePlayer, addClient, removeClient} = require('./reducers/Player')
+
+let lobbyMap = []
+let randMap = []
+let width = 15;
+let height = 15;
+
+function addMap(){
+  let crateTable=[]
+  for(let h = 0; h< height; h++){
+    let crateRow = []
+    for (let w = 0; w < width; w++){
+      let tile = false
+      if(h!==0 && w!==0 && h!==height-1 && w!==width-1 && (h%2==1 || w%2==1) ){
+        if(!(h===1 && w===1) && !(h===height-2 && w===width-2 ) && !(h===1 && w== width-2)
+        && !(h===height-2 && w===1) && !(h===2 && w===1) && !(h===1 && w===2) && !(h==height-2
+          && w=== width-3) && !(h===height-3 && w=== width-2) && !(h===height-2 &&w===2) && !(h===height-3 && w==1)
+          && ! (w===width-3 && h===1) && !(w===width-2 && h===2)){
+
+               let randNum = Math.floor(Math.random()*w)
+               let randNum1 = Math.floor(Math.random()*h)
+              if( randNum % 2 !== 0 || randNum1 % 2 !== 0){
+              tile = true
+              }
+            }
+      }
+        crateRow.push(tile)
+
+    }
+
+    crateTable.push(crateRow)
+
+  }
+  return crateTable
+}
+
+
 const listeners =  function( io, socket){
   console.log('socket id:', socket.id)
   socket.on('game_started', data =>{
     store.dispatch(addClient(socket.id))
+    if(!lobbyMap[data.myId-1])
+    lobbyMap[data.myId-1]= addMap()
+    io.emit('load_crates', {table: lobbyMap[data.myId-1]})
   })
 
   socket.on('disconnect', ()=>{
@@ -42,6 +81,9 @@ const listeners =  function( io, socket){
   })
   socket.on('client_delete_timer', (data)=>{
     io.emit('server_delete_timer', data)
+  })
+  socket.on('client_create_crates', (data)=>{
+    io.emit('server_create_crates', data)
   })
 
 }
